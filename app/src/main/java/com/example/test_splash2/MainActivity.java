64e34c7a291d78b6f33a1e610ac95a0416c87f43
pikaeb <151694264+pikaeb@users.noreply.github.com>
2023-11-22 22:32:20 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/test_splash2/MainActivity.java
+++ app/src/main/java/com/example/test_splash2/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("tagtag", "onCreate called. savedInstanceState: " + savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("tagtag","1");
 
